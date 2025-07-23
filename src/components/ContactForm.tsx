004405ae72ba6235d81e09ce7307d200f49e6798
GovanDBT <govan.dibotelo@gmail.com>
2025-07-23 12:09:45 +0200

    add recaptha

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,10 +1,82 @@
-import { FieldValues, useForm } from "react-hook-form";
+import { useForm } from "react-hook-form";
+import z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useRef, useState } from "react";
+import ReCAPTCHA from "react-google-recaptcha";
+import { ClipLoader } from "react-spinners";
+
+// zod validation schema
+const contactSchema = z.object({
+  name: z.string().min(1, "Fullname is required!"),
+  email: z.email("Invalid email address"),
+  subject: z.string().min(1, "Email subject is required!"),
+  message: z.string().min(1, "Message is required!"),
+  organization: z.string(),
+  phone: z.string().min(1, "Phone number is required!"),
+  token: z.string().min(1, "Please verify reCAPTCHA!"),
+});
+
+type contactFormData = z.infer<typeof contactSchema>;
 
 const ContactForm = () => {
-  const { register, handleSubmit } = useForm();
-  const onSubmit = (data: FieldValues) => console.log(data);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+    setValue,
+  } = useForm<contactFormData>({
+    resolver: zodResolver(contactSchema),
+    defaultValues: { token: "" },
+  });
+
+  const [success, setSuccess] = useState(false);
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const messageRef = useRef<HTMLDivElement>(null);
+
+  const onSubmit = async (data: contactFormData) => {
+    setLoading(true);
+    setSuccess(false);
+    setError(false);
+    try {
+      const res = await fetch("https://idealdomain.co.bw/mail.php", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data),
+      });
+
+      const result = await res.json();
+
+      if (result.success) {
+        setSuccess(true);
+        reset();
+      } else {
+        setError(true);
+      }
+    } catch (error) {
+      console.error("Error sending email:", error);
+      setError(true);
+    } finally {
+      setLoading(false);
+      window.scrollTo(0, 0);
+    }
+  };
+
   return (
     <form onSubmit={handleSubmit(onSubmit)} className="space-y-10">
+      <div ref={messageRef}>
+        {success && (
+          <div className="bg-green-100 text-green-700 px-4 py-4 rounded">
+            Message sent successfully!
+          </div>
+        )}
+        {error && (
+          <div className="bg-red-100 text-red-700 px-4 py-4 rounded">
+            Message failed to send!
+          </div>
+        )}
+      </div>
       <div className="grid md:grid-cols-2 gap-6">
         <div>
           <label htmlFor="name" className="form-label block mb-3 font-bold">
@@ -17,6 +89,9 @@ const ContactForm = () => {
             placeholder="your name..."
             id="name"
           />
+          {errors.name && (
+            <p className="text-red-500 mt-2">{errors.name.message}</p>
+          )}
         </div>
         <div>
           <label htmlFor="email" className="form-label block mb-3 font-bold">
@@ -29,6 +104,9 @@ const ContactForm = () => {
             placeholder="name@example.com"
             id="email"
           />
+          {errors.email && (
+            <p className="text-red-500 mt-2">{errors.email.message}</p>
+          )}
         </div>
       </div>
       <div className="grid md:grid-cols-2 gap-6">
@@ -43,17 +121,23 @@ const ContactForm = () => {
             placeholder="+267 ..."
             id="phone"
           />
+          {errors.phone && (
+            <p className="text-red-500 mt-2">{errors.phone.message}</p>
+          )}
         </div>
         <div>
-          <label htmlFor="org" className="form-label block mb-3 font-bold">
-            Organisation:
+          <label
+            htmlFor="organization"
+            className="form-label block mb-3 font-bold"
+          >
+            Organization:
           </label>
           <input
-            {...register("org")}
+            {...register("organization")}
             type="text"
             className="form-control bg-white text-primary w-full outline-0 py-3 px-2 rounded"
             placeholder="e.g. KPJ Drilling"
-            id="org"
+            id="organization"
           />
         </div>
       </div>
@@ -68,6 +152,9 @@ const ContactForm = () => {
           placeholder="e.g. Requesting a Quote"
           id="subject"
         />
+        {errors.subject && (
+          <p className="text-red-500 mt-2">{errors.subject.message}</p>
+        )}
       </div>
       <div>
         <label htmlFor="message" className="form-label block mb-3 font-bold">
@@ -81,16 +168,39 @@ const ContactForm = () => {
           rows={8}
           maxLength={200}
         />
-        <p className="text-sm mt-2 italic text-right">
-          Max of 200 characters allowed
-        </p>
+        <div className="flex items-center justify-between">
+          {errors.message && (
+            <p className="text-red-500 mt-2">{errors.message.message}</p>
+          )}
+          <p className="text-sm mt-2 italic text-right">
+            Max of 200 characters allowed
+          </p>
+        </div>
       </div>
+
+      <ReCAPTCHA
+        sitekey="6LeirYwrAAAAABgeZ_GB3OZ7_DhJO5_-UJcJczJU"
+        onChange={(token: string | null) => setValue("token", token || "")}
+      >
+        {errors.token && (
+          <p className="text-red-500 mt-2">{errors.token.message}</p>
+        )}
+      </ReCAPTCHA>
+
       <div className="place-self-center">
         <button
           type="submit"
-          className="bg-white text-primary rounded py-3 text-nowrap text-lg font-bold w-60 cursor-pointer hover:bg-secondary hover:text-white transition ease-in-out"
+          className="bg-white text-primary rounded py-3 text-nowrap text-lg font-bold w-60 cursor-pointer hover:bg-secondary hover:text-white transition ease-in-out flex justify-center items-center gap-3"
+          disabled={loading}
         >
-          Submit
+          <ClipLoader
+            color={"#0A2E57"}
+            loading={loading}
+            size={20}
+            aria-label="Loading Spinner"
+            data-testid="loader"
+          />
+          {loading ? "Sending..." : "Send Message"}
         </button>
       </div>
     </form>
